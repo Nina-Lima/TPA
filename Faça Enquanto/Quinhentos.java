4b583737f037274931de8c1b5095d14b6acca463
Nina-Lima <sabrina.lima96@etec.sp.gov.br>
2024-09-20 09:18:29 -0300

    Add files via upload

---
+++ Faça Enquanto/Quinhentos.java
@@ -0,0 +1,12 @@
+
+public class Quinhentos {
+	public static void main(String args[]) {
+		int i= 0;
+		while (i<501) {
+			System.out.println(i);
+			i+=2; //pulando de 2 para aparecer os números pares
+		}
+	}
+	
+
+}
