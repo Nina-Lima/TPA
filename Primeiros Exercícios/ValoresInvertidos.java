285816f0590d4673b2b866ed97a1cadbd7ed29ec
Nina-Lima <sabrina.lima96@etec.sp.gov.br>
2024-09-20 08:57:36 -0300

    Add files via upload

---
+++ Primeiros Exercícios/ValoresInvertidos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ValoresInvertidos {
+	public static void main (String args[]) {
+		Scanner leia = new Scanner(System.in);
+		int valorA, valorB, c;
+		System.out.println("Digite o valor A");
+		valorA= leia.nextInt();
+		System.out.println("Digite o valor B");
+		valorB= leia.nextInt();
+		c=valorA;
+		valorA= valorB;
+		valorB= c;
+		System.out.println("O valor A agora é: " +valorA);
+		System.out.println("O valor B agora é: " +valorB);
+	}leia.close();
+
+}
