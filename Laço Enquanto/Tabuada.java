2950628ab88ce80073f4f702db2e1d79b545af63
Nina-Lima <sabrina.lima96@etec.sp.gov.br>
2024-09-20 08:44:45 -0300

    Add files via upload

---
+++ Laço Enquanto/Tabuada.java
@@ -0,0 +1,25 @@
+
+public class Tabuada {
+	public static void main(String[] args) {
+		int n=1, r, i;
+		System.out.println("Tabuada do "+n+":");
+		while(n<10) {
+			i=1;
+			while (i <11) {
+				r=i*n;
+				System.out.println(n+" x "+i+" = "+r);
+				i++;
+			}
+			System.out.println( );
+			System.out.println("Tabuada do "+(n+1)+":");
+			i=1;
+			n++;
+			while(i<11) {
+				r=i*n;
+				System.out.println(n+" x "+i+" = "+r);
+				i++;
+			}
+		}
+	}
+
+}
